4c6fbde1f327d96f6dd9484e4d7b51fd54cbd23c
denisdulici <denisdulici@gmail.com>
2017-08-06 18:16:49 +0300

    added AdminLTE presenter

--- config/config.php
+++ config/config.php
@@ -9,6 +9,7 @@ return [
         'nav-tab' => \Nwidart\Menus\Presenters\Bootstrap\NavTabPresenter::class,
         'sidebar' => \Nwidart\Menus\Presenters\Bootstrap\SidebarMenuPresenter::class,
         'navmenu' => \Nwidart\Menus\Presenters\Bootstrap\NavMenuPresenter::class,
+        'adminlte' => \Nwidart\Menus\Presenters\Admin\AdminltePresenter::class,
     ],
 
     'ordering' => false,
